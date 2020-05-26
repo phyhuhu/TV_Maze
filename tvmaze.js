4f28821a87c9a8a70381d66512e62487f49bc668
Huhu <phyhwj@Wenjuns-MacBook-Air.local>
2020-05-26 12:40:41 -0500

    add files

---
+++ tvmaze.js
@@ -0,0 +1,135 @@
+/** Given a query string, return array of matching shows:
+ *     { id, name, summary, episodesUrl }
+ */
+
+
+/** Search Shows
+ *    - given a search term, search for tv shows that
+ *      match that query.  The function is async show it
+ *       will be returning a promise.
+ *
+ *   - Returns an array of objects. Each object should include
+ *     following show information:
+ *    {
+        id: <show id>,
+        name: <show name>,
+        summary: <show summary>,
+        image: <an image from the show data, or a default imege if no image exists, (image isn't needed until later)>
+      }
+ */
+async function searchShows(query) {
+  // TODO: Make an ajax request to the searchShows api.  Remove
+  // hard coded data.
+
+  let res = await axios.get("http://api.tvmaze.com/search/shows", 
+  {params: {
+      q: query
+  }});
+
+  let movies=res.data.map(x => {
+    return {
+      id: x.show.id,
+      name: x.show.name,
+      summary: x.show.summary,
+      image: x.show.image ? x.show.image.medium : "#"
+    }
+  });
+
+  // return [
+  //   {
+  //     id: 1767,
+  //     name: "The Bletchley Circle",
+  //     summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
+  //     image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+  //   }
+  // ]
+  return movies;
+}
+
+
+
+/** Populate shows list:
+ *     - given list of shows, add shows to DOM
+ */
+
+function populateShows(shows) {
+  const $showsList = $("#shows-list");
+  $showsList.empty();
+
+  for (let show of shows) {
+    let $item = $(
+      `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
+         <div class="card" data-show-id="${show.id}">
+         <img class="card-img-top" src="${show.image}">
+           <div class="card-body">
+             <h5 class="card-title">${show.name}</h5>
+             <p class="card-text">${show.summary}</p>
+             <button class="btn btn-primary">Episodes</button>
+           </div>
+         </div>
+       </div>
+      `);
+
+    $showsList.append($item);
+  }
+}
+
+
+/** Handle search form submission:
+ *    - hide episodes area
+ *    - get list of matching shows and show in shows list
+ */
+
+$("#search-form").on("submit", async function handleSearch (evt) {
+  evt.preventDefault();
+
+  let query = $("#search-query").val();
+  if (!query) return;
+
+  $("#episodes-area").hide();
+
+  let shows = await searchShows(query);
+
+  populateShows(shows);
+});
+
+
+/** Given a show ID, return list of episodes:
+ *      { id, name, season, number }
+ */
+
+async function getEpisodes(id) {
+  // TODO: get episodes from tvmaze
+  //       you can get this by making GET request to
+  //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
+
+  let res = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`);
+
+  // TODO: return array-of-episode-info, as described in docstring above
+  let episodes=res.data.map(x => {
+    return {
+      id: x.id,
+      name: x.name,
+      season: x.season,
+      number: x.number
+    }
+  });
+  return episodes;
+}
+
+$("#shows-list").on("click", async function(evt) {
+  evt.preventDefault();
+  let id = $(evt.target).closest(".Show").data("show-id");
+  let episodes = await getEpisodes(id);
+
+  const listEpisodes=$("#episodes-list");
+  listEpisodes.empty();
+
+  episodes.map(x => {
+    let episode=`<li> ${x.name} (season ${x.season}, episode ${x.number})</li>`;
+    listEpisodes.append(episode);
+  });
+
+  $("#episodes-area").show();
+
+});
